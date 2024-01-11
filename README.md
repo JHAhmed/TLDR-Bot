7676804840388b072704c12a4410cbaf21c9ae6d
Haneef Jamal <jamalhascientist@gmail.com>
2024-01-11 23:07:31 +0530

    added-style

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-# TLDR-Bot
+# TLDR Bot
 A discord bot built on Python that summarizes conversations in your server using ~~OpenAI's GPT 3.5 API~~ Google's Gemini Pro API.
 Bring your own BOT_TOKEN and API_KEY in an `.env` (or heck, plug them directly into the code!)
-This bot supports slash commands, use `/summarize {number} {style}` to TL;DR a number of messages in different styles!
+This bot supports slash commands, use `/tldr {number} {style}` to TL;DR a number of messages in different styles!
+
+- Exclude messages from users and bots by adding their user IDs to `exclude_list`
 
 Roadmap:
 - Add support OpenAI's GPT models
-- Add option to exlude users
-- Support for more messages
 - More styles?
--- bot.py
+++ bot.py
@@ -11,6 +11,8 @@ TOKEN = os.getenv('BOT_TOKEN')
 GENAI_API_KEY = os.getenv('GENAI_API_KEY')
 MY_GUILD = discord.Object(id=os.getenv('GUILD_ID'))
 
+exclude_list = []
+
 genai.configure(api_key=GENAI_API_KEY)
 model = genai.GenerativeModel('gemini-pro')
 
@@ -20,7 +22,13 @@ class Bot(discord.Client):
         self.tree = app_commands.CommandTree(self)
 
     async def setup_hook(self):
-        # Used to instantly sync commands to test guild, remove function in prod
+        '''Used to instantly sync commands to given guild, remove function in prod'''
+
+        # # Clear commands from guild
+        # self.tree.clear_commands(guild=None)
+        # await self.tree.sync()
+
+        # sync commands to guild
         self.tree.copy_global_to(guild=MY_GUILD)
         await self.tree.sync(guild=MY_GUILD)
 
@@ -30,10 +38,11 @@ client = Bot(intents=intents)
 async def summarize_messages (chat, variation=None) :
 
     variations = {
-        'normal': "normally as text: ",
+        'normal': "normally as a block of text: ",
         'pirate': "in the tone of a stereotypical pirate: ",
         'list': "as a bullet list of topics: ",
-        'dramatised': "but exagerrated and dramatised: "
+        'dramatised': "but exagerrated and dramatised: ",
+        'cowboy': "as a wild west Texan cowboy: "
     }
     
     response = model.generate_content("The following is chat from a discord server. Summarize the given text " + variations[variation] + chat)
@@ -45,7 +54,8 @@ async def summarize_messages (chat, variation=None) :
 async def send_summary(num_mesgs, channel, variation=None) :
     history = ""
     async for msg in channel.history(limit = num_mesgs):
-        history = f"{msg.author.display_name}: {msg.content} \n{history} "
+        if msg.author.id not in exclude_list:
+            history = f"{msg.author.display_name}: {msg.content} \n{history} "
 
     try:
         response = await summarize_messages(history, variation)
@@ -60,25 +70,14 @@ async def on_ready():
     print(f'Logged in as {client.user} (ID: {client.user.id})')
     print('---------------')
 
-# TODO - Remove Appa from conversations - Dad Bot#2189
-# TODO - Use tokenizer to break messages into chunks.
-
 @client.tree.command()
 @app_commands.describe(num_messages='Number of messages to summarize!')
 @app_commands.rename(num_messages='number')
 @app_commands.describe(variation='Spice up the summary!')
-async def summarize(interaction: discord.Interaction, num_messages: int, variation: Literal['normal', 'pirate', 'list', 'dramatised']):
+async def tldr(interaction: discord.Interaction, num_messages: int, variation: Literal['normal', 'pirate', 'list', 'dramatised', 'cowboy']):
     """Summarizes messages!"""
-    await interaction.response.send_message(f'Summarizing messages...', ephemeral=False)
-    response = await send_summary(num_messages, interaction.channel, variation)
-    await interaction.edit_original_response(content=response)
-
-@client.tree.command()
-@app_commands.describe(prompt='Enter prompt...')
-async def prompt(interaction: discord.Interaction, prompt: str):
-    """Unleash the power of LLMs in your messages!"""
-    await interaction.response.send_message(f'Generating...', ephemeral=False)
-    response = model.generate_content(prompt)
-    await interaction.edit_original_response(content=response.text)
+    await interaction.response.send_message(f'Summarizing messages...', ephemeral=True)
+    response = await send_summary(num_messages + 1, interaction.channel, variation)
+    await interaction.followup.send(response)
 
 client.run(TOKEN)
